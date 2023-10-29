011243b5c42dff4a69c31ed81d9876f890b1f5d8
Resatoznur <116165567+Resatoznur@users.noreply.github.com>
2023-10-29 18:45:57 +0300

    First Resat

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('MERHABA BEN ÜBEYDULLAH CEKLİ!');
+  res.send('MERHABA BEN REŞAT OZNUR GEYİK!');
 });
 
 const port = process.env.PORT || 3000;
